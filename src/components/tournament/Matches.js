b09403c3616c541d67cc19018b8f78be7be4ed6f
Sam Tyson <styson@gmail.com>
2022-03-19 10:43:47 -0500

    add match preview to find problems

--- src/components/tournament/Matches.js
+++ src/components/tournament/Matches.js
@@ -1,6 +1,7 @@
 import { Button, Form, Row } from 'react-bootstrap';
 import { Error } from '../styled/Error';
 import { getItem, putItem } from '../../dynamo/ApiCalls';
+import { Problem } from '../styled/Problem';
 import { useEffect, useState } from 'react';
 import Match from './Match';
 import React from 'react';
@@ -18,12 +19,15 @@ const newMatch = (scenarioList, round) => {
     noSide: true,
     scenarioList,
     round,
+    issues: '',
   };
 }
 
 const Matches = ({ round, scenarios, roundUpdate }) => {
   const [activePlayers, setActivePlayers] = useState([]);
   const [complete, setComplete] = useState(round.matchesComplete || false);
+  const [error, setError] = useState('');
+  const [problems, setProblems] = useState(['Preview the matches.']);
   const [matches, setMatches] = useState([]);
   const [scenarioList, setScenarioList] = useState([]);
 
@@ -93,6 +97,7 @@ const Matches = ({ round, scenarios, roundUpdate }) => {
   function reset() {
     showError('reset matches');
     setMatches([]);
+    setProblems([]);
     const rnd = { 
       ...round,
       matches,
@@ -122,10 +127,47 @@ const Matches = ({ round, scenarios, roundUpdate }) => {
     roundUpdate();
   }
 
-  const [error, setError] = useState('');
   function showError(error) {
     setError(error);
     setTimeout(() => {setError('')}, 3000);
+  }
+
+  const previewMatches = () => {
+    if (activePlayers.length === 0) {
+      showError(`Players are not set for matches.`);
+      return;
+    }
+
+    let problems = [];
+    let matches = [];
+    let match = {};
+
+    activePlayers.forEach((p, n) => {
+      if (n === 0 || !!(n && !(n%2))) {
+        match = newMatch([], round.round);
+        match['p1'] = p;
+        matches.push(match);
+      } else {
+        match['p2'] = p;
+      }
+    });
+
+    matches.forEach((m, n) => {
+      // problems.push(`${m.p1.name} vs. ${m.p2.name}`);
+      // have they played each other?
+      const rounds = Object.keys(m.p1.rounds);
+
+      rounds.forEach((r, n) => {
+        const rd = m.p1.rounds[r];
+        if (rd.opponent.sk === m.p1.sk || rd.opponent.sk === m.p2.sk) {
+          const issue = `${m.p1.name} played ${rd.opponent.name} in Round ${n+1}.`;
+          problems.push(issue);
+        }
+      });
+    });
+
+    showError(`Preview for ${activePlayers.length} players found ${problems.length} problems.`);
+    setProblems(problems);
   }
 
   return (
@@ -133,6 +175,7 @@ const Matches = ({ round, scenarios, roundUpdate }) => {
       <Form>
         <Form.Group className='mt-1 float-end' controlId={`completeMatches${round.round}`}>
           <Form.Check
+            className={`${matches.length === 0 ? 'd-none' : ''}`}
             type='switch' 
             label='Complete'
             checked={complete}
@@ -154,7 +197,7 @@ const Matches = ({ round, scenarios, roundUpdate }) => {
         <Button
           id='reset-matches'
           size='sm'
-          className={`flex-grow-1 me-1 mb-2 ${complete ? 'd-none' : ''}`}
+          className={`flex-grow-1 me-1 mb-2 ${complete || problems.length > 0 ? 'd-none' : ''}`}
           variant='outline-secondary'
           onClick={reset}
           disabled={complete}
@@ -162,9 +205,17 @@ const Matches = ({ round, scenarios, roundUpdate }) => {
           Reset Matches
         </Button>
         <Button
+          className={`flex-grow-1 me-1 mb-2 ${complete ? 'd-none' : ''}`}
+          size='sm'
+          variant='outline-primary'
+          onClick={previewMatches}
+        >
+          Preview Matches
+        </Button>
+        <Button
           id='generate-matches'
           size='sm'
-          className={`flex-grow-1 ms-1 mb-2 ${complete ? 'd-none' : ''}`}
+          className={`flex-grow-1 ms-1 mb-2 ${complete || problems.length > 0 ? 'd-none' : ''}`}
           variant='outline-success'
           onClick={generate}
           disabled={complete}
@@ -175,6 +226,14 @@ const Matches = ({ round, scenarios, roundUpdate }) => {
       <Error>
         {error}
       </Error>
+      <Problem
+        className={`${problems.length === 0 ? 'd-none' : ''}`}
+      >
+        {problems && Object.keys(problems).map(k => {
+          console.log(`${problems[k]}`)
+          return <li>{problems[k]}</li>
+        })}
+      </Problem>
     </Row>
   );
 }
